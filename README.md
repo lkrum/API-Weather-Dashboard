7ed94997feb62a2fee638731f964ce5b989439d6
lkrum <leahkrumholz73@gmail.com>
2023-04-16 01:21:05 -0500

    after 3 hours I finally got my local storage to save and display an array of cities

---
+++ README.md
@@ -0,0 +1,24 @@
+# API Weather Dashboard
+
+## Description
+
+
+![Home](./assets/images/main.png)
+![Question](./assets/images/question.png)
+![HighScores](./assets/images/highscores.png)
+
+Link to live page: 
+
+## Installation
+N/A
+
+## Usage
+
+
+## Credits 
+
+- [W3schools](https://www.w3schools.com/jsref/met_loc_assign.asp)
+- [StackOverflow](https://stackoverflow.com/questions/65908096/how-can-i-store-multiple-values-inside-one-localstorage-key)
+
+## License
+MIT License
--- assets/script.js
+++ assets/script.js
@@ -1,10 +1,10 @@
 // GIVEN a weather dashboard with form inputs
-  // find form on bootstrap
+// find form on bootstrap
 // WHEN I search for a city
-  // need to save search history in local storage
+// need to save search history in local storage
 // THEN I am presented with current and future conditions for that city and that city is added to the search history
-  // connect to weather API
-  // need to take search history from local storage and display it on page
+// connect to weather API
+// need to take search history from local storage and display it on page
 
 // WHEN I view current weather conditions for that city
 // THEN I am presented with the city name, the date, an icon representation of weather conditions, the temperature, the humidity, and the wind speed
@@ -14,33 +14,66 @@
 // THEN I am again presented with current and future conditions for that city
 
 // variable statements
+var searchInput = $('#search-input')
 var searchFormEl = $('#search');
 var searchBtn = $('#search-btn')
 var searchHistoryEl = $('#search-history-container');
-var searchHistory;
+var searchList = $('#search-list');
+var historyArray = [];
 var city;
 
 
 // variables
 var apiKey = "40b10aa426a06b771a72b081e7b57995";
-var queryURL = "http://api.openweathermap.org/data/2.5/weather?q=" + city + "&appid=" + apiKey;
+var queryURL = "https://api.openweathermap.org/data/2.5/weather?q=" + city + "&appid=" + apiKey;
 
-// search bar function
-$('#search-btn').click(function() {
-  var city = $('#search-input').val();
-  localStorage.setItem('city', city);
 
+function renderHistory() {
+  searchList.text('');
+  city = $('#search-input').val();
+  // var searchHistory = historyArray[i];
+  localStorage.setItem('city', JSON.stringify(historyArray));
+  // for-loop to create new list elements with each search input
+  for (var i = 0; i < historyArray.length; i++) {
+    var searchHistory = historyArray[i];
+    var li = $('<li>');
+    li.text(searchHistory);
+    li.attr('history-index', i);
+    searchList.append(li)
+  }
+}
+
+// get stored search history from local storage
+function init() {
+  var storedHistory = JSON.parse(localStorage.getItem('city'));
+  storedHistory.push(city);
+  // localStorage.setItem('city', JSON.stringify(historyArray));
+  renderHistory();
+}
+
+// search bar event listener function
+$('#search-btn').click(function(event) {
+  event.preventDefault();
+  // city = $('#search-input').val();
+  var searchText = searchInput.val();
+  historyArray.push(searchText);
+  searchInput.value = '';
+
+  renderHistory();
 });
-
 
 // api function
 function getApi() {
-fetch(queryURL)
-  .then(function(response) {
-    return response.json();
-  })
-  .then(function(data) {
-    console.log(data)
-});
+  fetch(queryURL)
+    .then(function (response) {
+      return response.json();
+    })
+    .then(function () {
+    });
 }
 
+
+// function searchApi(query,) {
+//   var locQueryUrl = 'https://www.loc.gov/search/?fo=json';
+
+// fetchButton.addEventListener('click', getApi);
